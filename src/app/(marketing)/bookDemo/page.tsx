da9a7beb56d41ac8691026f82916d9e9f515be10
Gregg Trimarchi <gttrimarchig@gmail.com>
2025-01-18 00:18:55 -0700

    added Google analytics 4

--- src/app/(marketing)/bookDemo/page.tsx
+++ src/app/(marketing)/bookDemo/page.tsx
@@ -1,25 +1,28 @@
-//import Link from "next/link"
+'use server'
 
-//import { cn } from "@/app/lib/utils"
-//import { buttonVariants } from "@/app/components/ui/button"
-//import { Icons } from "@/app/components/icons"
 import NavBar from "@/app/_components/navbar"
+import { GoogleAnalytics } from "@next/third-parties/google"
+import Head from "next/head"
 
 export const metadata = {
   title: "Let's Talk! | AutoClient",
 }
 
 export default function ConversationPage() {
+  const isProd = process.env.NODE_ENV === 'production';
   return (
     <div className="flex flex-col">
-        <NavBar/>
-        <div className="flex justify-center">
-          <div className="flex flex-col my-16 py-2 bg-white w-[95%] rounded-md ">
-            {/* <!-- Google Calendar Appointment Scheduling begin --> */}
-            <iframe src="https://calendar.google.com/calendar/appointments/schedules/AcZssZ2PObhQu80lW4_m0TiCE3QYQG91EJmoljVq1Nln-LpGRoZgUAEi3J17T-kO7dHzSGjczWwSDzdD?gv=true" style={{border: 0}} width="100%" height="700"></iframe>
-            {/* <!-- end Google Calendar Appointment Scheduling --> */}
-          </div>
+      <Head>
+        {isProd && <GoogleAnalytics gaId="G-C65FYE6C0T" />}    
+      </Head>
+      <NavBar/>
+      <div className="flex justify-center">
+        <div className="flex flex-col my-16 py-2 bg-white w-[95%] rounded-md ">
+          {/* <!-- Google Calendar Appointment Scheduling begin --> */}
+          <iframe src="https://calendar.google.com/calendar/appointments/schedules/AcZssZ2PObhQu80lW4_m0TiCE3QYQG91EJmoljVq1Nln-LpGRoZgUAEi3J17T-kO7dHzSGjczWwSDzdD?gv=true" style={{border: 0}} width="100%" height="700"></iframe>
+          {/* <!-- end Google Calendar Appointment Scheduling --> */}
         </div>
+      </div>
         
     </div>
   )
